ce83b28489807174a0ba18c680929c1ab3341dc6
Pavel Krayzel <pavel.krayzel@skyscanner.net>
2017-11-02 21:51:55 +0000

    First version of form inputs aligned with buttons, form action simulation.

--- src/app.py
+++ src/app.py
@@ -3,12 +3,21 @@ from flask import Flask, render_template
 
 # create the application object
 app = Flask(__name__)
+HOME_PAGE = 'index.html'
 
 
 # use decorators to link the function to a url
 @app.route('/')
 def home():
-    return render_template('index.html')  # render a template
+    return render_template(HOME_PAGE)
+
+
+@app.route('/<email>', methods=["POST"])
+def send(email):
+    # TODO - send email
+    print('Sending an email to: {}'.format(email))
+
+    return render_template(HOME_PAGE)
 
 
 # start the server with the 'run()' method
