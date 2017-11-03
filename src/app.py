274ca11ed0b1f4113ae0d84498589a9ee4691b00
Pavel Krayzel <pavel.krayzel@skyscanner.net>
2017-11-03 21:14:25 +0000

    Configuration from config file, sending email through gmail (simple templates)

--- src/app.py
+++ src/app.py
@@ -1,13 +1,21 @@
-# import the Flask class from the flask module
 from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+from logging import config
+import logging
 
-# create the application object
-app = Flask(__name__)
+# constants
 HOME_PAGE = 'index.html'
 SENT = 'sent.html'
+DRINK = 'drink'
+SMELL = 'smell'
+
+app = Flask(__name__)
+app.config.from_object('config')
+mail = Mail(app)
+
+config.fileConfig('logging.conf')
 
 
-# use decorators to link the function to a url
 @app.route('/')
 def home():
     return render_template(HOME_PAGE)
@@ -15,11 +23,22 @@ def home():
 
 @app.route('/send', methods=["POST"])
 def send_email():
-    # email = request.form['email_smell']
-    # return 'Send email: ' % email
+    email = request.form['email']
+    template = request.form['template']
+
+    msg = Message('Hello', sender='intervention.ninja@gmail.com', recipients=email.split(','))
+
+    if template == DRINK:
+        msg.body = "Hey, could you please stop drinking so much? Cheers!"
+    elif template == SMELL:
+        msg.body = "Hey, have you ever seen shower? Someone thinks that you smell like sh*t! Cheers!"
+
+    logging.info('Sending email to recipient: {} with template: {}'.format(email, template))
+    mail.send(msg)
+
     return render_template(SENT)
 
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, threaded=True, debug=True)
+    app.run(host="0.0.0.0", port=5000, threaded=app.config['THREADED'], debug=app.config['DEBUG'])
---
+++ src/config.py
@@ -0,0 +1,9 @@
+DEBUG = False
+THREADED = True
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USERNAME = 'intervention.ninja@gmail.com'
+MAIL_PASSWORD = 'Coffee900'
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
