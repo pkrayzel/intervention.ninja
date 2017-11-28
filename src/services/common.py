9b86c7250b636b6e8af2d01e4cc5e5c929e9aaf5
Pavel Krayzel <pavel.krayzel@skyscanner.net>
2017-11-28 22:11:49 +0000

    Added tests to common service, non flask templating for emails

--- src/services/common.py
+++ src/services/common.py
@@ -1,6 +1,6 @@
-from services.mail import MailService
-from services import dao
-from services import common
+from src.services.mail import MailService
+from src.services.template import TemplateServiceS3
+from src.services import dao
 import logging
 import json
 
@@ -8,6 +8,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 mail = MailService()
+mail_template_service = TemplateServiceS3('www.intervention.ninja', 'emails/')
 
 KEY_EMAIL = 'email'
 KEY_TEMPLATE = 'template'
@@ -19,6 +20,9 @@ MAX_EMAILS_PER_EMAIL_PER_MINUTE = 1
 
 MILLISECONDS_PER_MINUTE = 60 * 1000
 
+MAIL_SUBJECT = 'Intervention Ninja - personal message'
+MAIL_SENDER = 'intervention.ninja@gmail.com'
+
 
 def validate_send_email(body, context):
     logger.info('validate_send_email: {}, {}'.format(body, context))
@@ -26,44 +30,46 @@ def validate_send_email(body, context):
     if KEY_EMAIL not in body \
         or KEY_TEMPLATE not in body \
         or KEY_SOURCE_IP not in context:
-        return construct_response_bad_request()
+        return _construct_response_bad_request()
 
     email = body[KEY_EMAIL]
     template = body[KEY_TEMPLATE]
     source_ip = context[KEY_SOURCE_IP]
 
     if template not in SUPPORTED_TEMPLATES:
-        return construct_response(400, "Given template value is not supported.")
+        return _construct_response(400, "Given template value is not supported.")
 
     # check whether from given ip address hasn't been sent email in last 1 minute
     count = dao.get_count_for_ip_address(source_ip, MILLISECONDS_PER_MINUTE)
 
     if count >= MAX_EMAILS_PER_IP_PER_MINUTE:
-        return construct_response(429, "Limit of requests from IP address per minute exceeded.")
+        return _construct_response(429, "Limit of requests from IP address per minute exceeded.")
 
     # check whether from given ip address hasn't been sent email in last 1 minute
     count = dao.get_count_for_email(email, MILLISECONDS_PER_MINUTE)
 
     if count >= MAX_EMAILS_PER_EMAIL_PER_MINUTE:
-        return construct_response(429, "Limit of requests for single email per minute exceeded.")
+        return _construct_response(429, "Limit of requests for single email per minute exceeded.")
 
-    mail.send_mail(email, template)
+    content = mail_template_service.render_template('{}.txt'.format(template))
+    content_html = mail_template_service.render_template('{}.html'.format(template))
+
+    mail.send_mail(MAIL_SUBJECT, MAIL_SENDER, email, content, content_html)
 
     dao.store_ip_address(source_ip)
     dao.store_email(email, template)
-
-    return construct_response(200, "")
-
-
-def construct_response_bad_request():
-    return common.construct_response(400, "Bad request. All required input parameters should be provided.")
+    return _construct_response(200, "")
 
 
-def construct_response_server_error():
-    return common.construct_response(500, "Something is wrong!")
+def _construct_response_bad_request():
+    return _construct_response(400, "Bad request. All required input parameters should be provided.")
 
 
-def construct_response(status_code, message, response_body=None):
+def _construct_response_server_error():
+    return _construct_response(500, "Something is wrong!")
+
+
+def _construct_response(status_code, message, response_body=None):
     result = {
         'status_code': status_code,
         'body': message if response_body is None else json.dumps(response_body),
--- src/services/mail.py
+++ src/services/mail.py
@@ -10,15 +10,18 @@ class MailService:
     def __init__(self):
         self._mail_service = None
 
-    def send_mail(self, email, template):
-        msg = Message('Intervention Ninja - personal message',
-                      sender='intervention.ninja@gmail.com',
-                      recipients=[email])
-        msg.body = render_template('emails/{}.txt'.format(template))
-        msg.html = render_template('emails/{}.html'.format(template))
+    def send_mail(self, subject, sender, recipient, body, body_html):
+        try:
+            msg = Message(subject, sender, recipients=[recipient])
+            msg.body = body
+            msg.html = body_html
 
-        logger.info('Sending email to recipient: {} with template: {}'.format(email, template))
-        self._get_mail().send(msg)
+            logger.info('Sending email to recipient: {}'.format(recipient))
+            self._get_mail().send(msg)
+            return True
+        except Exception as exception:
+            logger.error('Sending email failed with exception: {}'.format(exception))
+            return False
 
     def _get_mail(self):
         if self._mail_service is None:
