92108b963e91f64e36377d06716cc7be5a01c4de
Pavel Krayzel <pavel.krayzel@skyscanner.net>
2017-12-28 13:43:40 +0100

    Lambda deployment finalization, error response mapping

--- src/services/common.py
+++ src/services/common.py
@@ -43,8 +43,8 @@ def validate_send_email(body, context):
 
     if template not in SUPPORTED_TEMPLATES:
         logger.warning('Template is not supported: {}. Should be one of: {}'
-                     .format(template, SUPPORTED_TEMPLATES))
-        return _construct_response(400, "Given template value is not supported.")
+                       .format(template, SUPPORTED_TEMPLATES))
+        return construct_response_bad_request("Given template value is not supported.")
 
     # check whether from given ip address hasn't been sent email in last 1 minute
     count = dao.get_count_for_ip_address(source_ip, MILLISECONDS_PER_MINUTE)
@@ -52,7 +52,7 @@ def validate_send_email(body, context):
     if count >= MAX_EMAILS_PER_IP_PER_MINUTE:
         logger.warning('Maximum emails from IP: {} per minute achieved: {}'
                        .format(source_ip, count))
-        return _construct_response(429, "Limit of requests from IP address per minute exceeded.")
+        return construct_response_limit_exceeded("Limit of requests from IP address per minute exceeded.")
 
     # check whether from given ip address hasn't been sent email in last 1 minute
     count = dao.get_count_for_email(email, MILLISECONDS_PER_MINUTE)
@@ -60,7 +60,7 @@ def validate_send_email(body, context):
     if count >= MAX_EMAILS_PER_EMAIL_PER_MINUTE:
         logger.warning('Maximum emails {} per minute achieved: {}'
                        .format(email, count))
-        return _construct_response(429, "Limit of requests for single email per minute exceeded.")
+        return construct_response_limit_exceeded("Limit of requests for single email per minute exceeded.")
 
     content_html = mail_template_service.render_template('{}.html'.format(template))
 
@@ -68,25 +68,41 @@ def validate_send_email(body, context):
 
     dao.store_ip_address(source_ip)
     dao.store_email(email, template)
-    return _construct_response(200, "")
+    return _construct_response_success()
 
 
-def construct_response_bad_request():
-    return _construct_response(400, "Bad request. All required input parameters should be provided.")
+def construct_response_bad_request(
+        message="Bad request. All required input parameters should be provided."):
+    return _construct_response_error(400, message, "BadRequest")
+
+
+def construct_response_limit_exceeded(message):
+    return _construct_response_error(429, message, "LimitExceeded")
 
 
 def construct_response_server_error():
-    return _construct_response(500, "Something is wrong!")
+    return _construct_response_error(500, "Something is wrong!", "GeneralError")
 
 
-def _construct_response(status_code, message, response_body=None):
+def _construct_response_error(status_code, message, code):
     result = {
         'status_code': status_code,
-        'body': message if response_body is None else json.dumps(response_body),
+        'body': {'code': code, 'message': message},
         'headers': {
             'Content-Type': 'application/json',
         },
     }
-    logger.info('constructing response - {}'.format(result))
+    logger.info('constructing error response - {}'.format(result))
+    return result
 
+
+def _construct_response_success(response_body=None):
+    result = {
+        'status_code': 200,
+        'body': '' if response_body is None else json.dumps(response_body),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+    logger.info('constructing success response - {}'.format(result))
     return result
