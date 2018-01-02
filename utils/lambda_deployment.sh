322451b5504efebd177157bb8ca7bd3dcaf7dc1e
Pavel Krayzel <pavel.krayzel@skyscanner.net>
2018-01-02 22:31:23 +0100

    Deployment support cloudformation template, small changes in deployment

--- utils/lambda_deployment.sh
+++ utils/lambda_deployment.sh
@@ -4,8 +4,8 @@ cp -rf ~/hw/lib/python3.6/site-packages/* dist
 cp ../src/__init__.py dist/.
 cp ../src/intervention_ninja.py dist/.
 cp -rf ../src/services dist
-cd dist && zip -r ../dist.zip . && cd ..
+cd dist && zip -r ../lambda_dist.zip . && cd ..
 
-aws s3 cp dist.zip s3://deploymentsupport/www.intervention.ninja/
+aws s3 cp --region us-east-1 lambda_dist.zip s3://intervention-ninja-deployment-support/
 
-aws lambda update-function-code --profile private_developer --region eu-west-1 --function-name intervention_ninja_mail_sender --s3-bucket deploymentsupport --s3-key www.intervention.ninja/dist.zip --publish
+aws lambda update-function-code --profile private_developer --region us-east-1 --function-name intervention_ninja_email_sender --s3-bucket intervention-ninja-deployment-support --s3-key lambda_dist.zip --publish
